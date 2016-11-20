1b341d2dcb8330361356ec668197ffcbd90021bc
cs280aby <abbytabbabby@gmail.com>
2016-11-20 18:22:16 +0800

    Update topic_classification.py

--- topic_classification.py
+++ topic_classification.py
@@ -6,24 +6,27 @@ import glob
 import pandas as pd
 import numpy as np
 import multiprocessing.pool as mp
-import threading as td
+from collections import Counter
 
 CLEANUP_REGEX = re.compile("(^Path: )|(^From: )|(^Newsgroups: )|(^Date: )|(^Organization: )|(^Lines: )|(^Message-ID: )|(^References: )|(^NNTP-Posting-Host: )")
 WORD_REGEX = re.compile("[a-zA-Z]{5,15}[,|.]*$")
 CWD = os.getcwd()
 WAIT_TIME = 30
+TESTING = True
 # set BINARY to True if you wish output to be 0/1 instead of word frequency count
-BINARY = True
+BINARY = False
 # parameters for input file processing
 # change PROCESSED_INPUT_DIR to the directoy where you want to place the processed input files
-PROCESSED_INPUT_DIR = "processed-test-n3"
+PROCESSED_INPUT_DIR = "processed-test-n2"
 # setting PRE_PROCESS_INPUT = True will process the intput files AND proceed to produce SVM input files using the 
 #    in INDEX_FILE and N_GRAM configuration
 PRE_PROCESS_INPUT = False
-N_GRAM = 3
+N_GRAM = 2
 
 # parameters for creating svm input files based on given features
 # use this for pool of N-gram features
+GET_INDEX = False
+MIN_WORD_FREQUENCY = 50
 INDEX_FILE = "indexes_n_all.txt"
 # this contains the directories of the processed files based on given N-gram
 PROCESSED_INPUT_DIR_N = [
@@ -32,16 +35,17 @@ PROCESSED_INPUT_DIR_N = [
     "processed-test-n3" 
 ]
 # change OUTPUT_DIR to the directoy where you want to place the svm input files
-OUTPUT_DIR = "pre_svm-test"
+OUTPUT_DIR = "pre_svm-test-4-1"
 # Set ALL_N to True if all input files for all N are already procesed
-ALL_N = True
+ALL_N = False
 
 def processInputFiles(sub_dir_list):
     #lfiles = glob.glob(os.path.join(INPUT_DIR, "*"))
     for sub_dir in sub_dir_list:
         lfiles = os.listdir(os.path.join(INPUT_DIR, sub_dir))
         #print lfiles
-        os.mkdir(os.path.join(PROCESSED_INPUT_DIR, INPUT_DIR, sub_dir))
+        if not os.path.exists(os.path.join(PROCESSED_INPUT_DIR, INPUT_DIR, sub_dir)):
+            os.mkdir(os.path.join(PROCESSED_INPUT_DIR, INPUT_DIR, sub_dir))
         for lfile in lfiles:
             with open(os.path.join(INPUT_DIR, sub_dir, lfile)) as f:
                 print "Processing input file for category %s, file %s.. " % (INPUT_DIR, lfile)
@@ -184,7 +188,8 @@ def preProcessInputFiles(cat_list, sub_dir_list):
         print "Processing input file for category %s.. " % (cat)
         INPUT_DIR = cat
         LABEL = str(label_list)
-        os.mkdir(os.path.join(PROCESSED_INPUT_DIR, INPUT_DIR)) 
+        if not os.path.exists(os.path.join(PROCESSED_INPUT_DIR, INPUT_DIR)):
+            os.mkdir(os.path.join(PROCESSED_INPUT_DIR, INPUT_DIR)) 
         processInputFiles(sub_dir_list)
         label_list+=1
         
@@ -252,23 +257,51 @@ def main():
         "talk.politics.misc", 
         "talk.religion.misc"
      ]
-    cat_list = ["alt.atheism"]
-    cat_list = ["cat1", "cat2"]
+    #cat_list = ["alt.atheism", "comp.graphics", "comp.os.ms-windows.misc"]
+    #cat_list = ["cat1", "cat2"]
     # divide each input file folder to 4 sub-folders, 250 files each for multiprocessing
     # use folders 1-3 for training, folder 4 for testing
     # edit sub_dir_list below to indicate list of folders to be processed
-    sub_dir_list = ["1"]
-    
- 
-    if PRE_PROCESS_INPUT:
+    sub_dir_list = ["1", "2", "3"]
+    if not os.path.exists(PROCESSED_INPUT_DIR):
         os.mkdir(PROCESSED_INPUT_DIR)
-        if not os.path.exists(OUTPUT_DIR):
-           os.mkdir(OUTPUT_DIR)
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+
+        
+    if TESTING:
+        sub_dir_list = ["4"]
+        preProcessInputFiles(cat_list, sub_dir_list)
+
+    if PRE_PROCESS_INPUT:
         preProcessInputFiles(cat_list, sub_dir_list)
         preGetFeatures(cat_list, sub_dir_list)
+        
+    if GET_INDEX:
+        print "Getting indexes above minimum frequency.."
+        words = []
+        for category in cat_list:
+            for sub_dir in sub_dir_list:
+                lfiles = glob.glob(os.path.join(PROCESSED_INPUT_DIR, category,sub_dir, "*"))
+                for lfile in lfiles:
+                    with open(lfile) as f:
+                        temp = f.read().splitlines()
+                    words+=temp
+                    f.close()
+                
+        feature_counts = Counter(words)
+        feature_list =  [word for word, count in feature_counts.items() if count > MIN_WORD_FREQUENCY]
+        print "New feature vector length: %d" % (len(feature_list))
+        f = open(os.path.join(INDEX_FILE), "w")
+        f.writelines(["%s\n" % feature for feature in feature_list])
+        f.close()
+        return
     
     with open(INDEX_FILE) as f:
         feature_list = f.read().splitlines()
+    time.sleep(10)
+    print "Feature vector length: %d" % (len(feature_list))
+    
     preConstructTable(cat_list, feature_list, sub_dir_list)
      
 
